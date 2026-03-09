db2b35db49fb122d6cc93ff98364cb4225d8feb4
David Chen <david.chen@choiceform.com>
2026-03-09 10:38:22 +0800

    feat: add request_id to context

--- lib/atomemo_plugin_sdk/context.ex
+++ lib/atomemo_plugin_sdk/context.ex
@@ -1,10 +1,11 @@
 defmodule AtomemoPluginSdk.Context do
   @derive {JSON.Encoder, only: [:organization_id]}
 
-  defstruct [:__hub_client__, :organization_id]
+  defstruct [:__hub_client__, :organization_id, :request_id]
 
   @type t :: %__MODULE__{
           __hub_client__: pid() | atom(),
-          organization_id: String.t()
+          organization_id: String.t() | nil,
+          request_id: String.t() | nil
         }
 end
