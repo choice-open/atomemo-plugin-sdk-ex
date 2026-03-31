966f0e767467fe19a66adb28e3703c43e83ecc18
Kenton Wang <smartepsh@gmail.com>
2026-03-31 15:52:20 +0800

    feat: validate and ensure boolean parameters correctly
    Add default validation in boolean parameter definition and strict
    type checking in boolean validator to ensure only boolean values are
    accepted.

--- lib/atomemo_plugin_sdk/parameter_validator/boolean.ex
+++ lib/atomemo_plugin_sdk/parameter_validator/boolean.ex
@@ -2,4 +2,13 @@ defmodule AtomemoPluginSdk.ParameterValidator.Boolean do
   @moduledoc false
 
   use AtomemoPluginSdk.ParameterValidator
+
+  @impl true
+  def validate(_definition, value, _opts) do
+    if is_boolean(value) do
+      {:ok, value}
+    else
+      {:error, [%{path: :type, message: "must be a boolean."}]}
+    end
+  end
 end
---
+++ test/atomemo_plugin_sdk/parameter_validator/boolean_test.exs
@@ -0,0 +1,21 @@
+defmodule AtomemoPluginSdk.ParameterValidator.BooleanTest do
+  use ExUnit.Case, async: true
+
+  alias AtomemoPluginSdk.ParameterDefinition.Boolean, as: PDBoolean
+  alias AtomemoPluginSdk.ParameterValidator.Boolean, as: PVBoolean
+
+  describe "validate/3" do
+    test "returns ok for boolean true" do
+      assert {:ok, true} = PVBoolean.validate(%PDBoolean{}, true, path: [:default])
+    end
+
+    test "returns ok for boolean false" do
+      assert {:ok, false} = PVBoolean.validate(%PDBoolean{}, false, path: [:default])
+    end
+
+    test "returns error when value is not boolean" do
+      assert {:error, [%{path: :type, message: "must be a boolean."}]} =
+               PVBoolean.validate(%PDBoolean{}, 1, path: [:default])
+    end
+  end
+end
