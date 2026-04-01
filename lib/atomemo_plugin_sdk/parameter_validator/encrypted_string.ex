537caa06a9bffb79516a2d326c77aa15dc84ba95
Kenton Wang <smartepsh@gmail.com>
2026-04-02 00:36:14 +0800

    feat: add encrypted string validator with type checking
    Add EncryptedString validator implementation to accept only string
    values and return descriptive error when input is not a
    string. Includes tests for both valid and invalid cases.

--- lib/atomemo_plugin_sdk/parameter_validator/encrypted_string.ex
+++ lib/atomemo_plugin_sdk/parameter_validator/encrypted_string.ex
@@ -2,4 +2,13 @@ defmodule AtomemoPluginSdk.ParameterValidator.EncryptedString do
   @moduledoc false
 
   use AtomemoPluginSdk.ParameterValidator
+
+  @impl true
+  def validate(_definition, value, _opts) when is_binary(value) do
+    {:ok, value}
+  end
+
+  def validate(_definition, _value, _opts) do
+    {:error, %{path: :type, message: "must be a string."}}
+  end
 end
--- test/atomemo_plugin_sdk/parameter_validator/encrypted_string_test.exs
+++ test/atomemo_plugin_sdk/parameter_validator/encrypted_string_test.exs
@@ -5,10 +5,15 @@ defmodule AtomemoPluginSdk.ParameterValidator.EncryptedStringTest do
   alias AtomemoPluginSdk.ParameterValidator.EncryptedString, as: PVEncryptedString
 
   describe "validate/3" do
-    test "uses default validator behavior and returns original value" do
+    test "returns ok when value is a string" do
       value = "anything"
 
       assert {:ok, ^value} = PVEncryptedString.validate(%PDEncryptedString{}, value, [])
+    end
+
+    test "returns error when value is not a string" do
+      assert {:error, %{path: :type, message: "must be a string."}} =
+               PVEncryptedString.validate(%PDEncryptedString{}, 123, [])
     end
   end
 end
